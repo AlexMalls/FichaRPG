c90a143d6799848d0b9777d04675838c00e5e543
Alexandre Mallorca <99052118+AlexMalls@users.noreply.github.com>
2026-05-05 18:35:15 -0300

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "FichaRPG",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body style={{ margin: 0, padding: 0 }}>
+        {children}
+      </body>
+    </html>
+  );
+}
