a5f8cf8d57fb05f6108a336f17fc5831cc692c4f
Alexandre Mallorca <99052118+AlexMalls@users.noreply.github.com>
2026-05-27 10:47:23 -0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,13 @@
 export const metadata = {
-  title: 'Fichas RPG',
-  icons: {
-    icon: '/images/logo.png',
-  },
+  title: "FichaRPG",
 };
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body style={{ margin: 0, padding: 0 }}>
+        {children}
+      </body>
+    </html>
+  );
+}
